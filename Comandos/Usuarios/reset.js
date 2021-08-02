b3e94d2cb4882b75e1eed1206c243433209999e7
JanVGX <83541534+JanVGX@users.noreply.github.com>
2021-08-03 00:45:54 +0200

    Update reset.js

--- Comandos/Usuarios/reset.js
+++ Comandos/Usuarios/reset.js
@@ -8,7 +8,7 @@ code: `
 
   $color[GREEN]
 
-  $description[Okay $username, I have reset the Global Chat values, reconfigure it with **c/setChannel** y **c/setInvite**
+  $description[Okay $username, I have reset the Global Chat values, reconfigure it with **c/setChannel** y **c!setInvite**
 
   
 
@@ -16,11 +16,11 @@ code: `
 
   $resetServerVar[chatinvite]
 
-  $onlyIf[$getServerVar[chatchannel]!=Sin establecer;{color: ff0000}{description: Hey!At the moment there is no channel established for Global Chat. Set it up with **c/setChannel**}]]
+  $onlyIf[$getServerVar[chatchannel]!=Sin establecer;{color: ff0000}{description: Hey!At the moment there is no channel established for Global Chat. Set it up with **c!setChannel**}]]
 
   $onlyBotPerms[manageserver;managemessages;sendmessages;viewchannel;managechannels;{description: Hey!You must give me more permissions to be able to do that.}{color: ff0000}]
 
-  $onlyPerms[admin;{description: ¡Hey! Debes ser administrador del servidor para utilizar este comando.}{color: ff0000}]
+  $onlyPerms[admin;{description: Hey!You must be a server administrator to use this command.}{color: ff0000}]
 
   $suppressErrors
 
