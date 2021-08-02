b9f419099c5e4a8f7ee5e3935810fad35ccf3af1
JanVGX <83541534+JanVGX@users.noreply.github.com>
2021-08-03 00:26:16 +0200

    Update setInvite.js

--- Comandos/Usuarios/setInvite.js
+++ Comandos/Usuarios/setInvite.js
@@ -16,7 +16,7 @@ code: `
 
   $argsCheck[>1;{description: Hey!You must indicate the invitation after the trigger (preferably permanent)}{color: ff0000}]
 
-  $onlyBotPerms[manageserver;managemessages;sendmessages;viewchannel;managechannels;{description: ¡Hey! Debes darme más permisos para poder hacer eso.}{color: ff0000}]
+  $onlyBotPerms[manageserver;managemessages;sendmessages;viewchannel;managechannels;{description: Hey!You must give me more permissions to be able to do that.}{color: ff0000}]
 
   $onlyPerms[admin;{description: ¡Hey! Debes ser administrador del servidor para utilizar este comando.}{color: ff0000}]
 
