58de0fcb63820bb20ff4a465eac4c8af4c7000f5
JanVGX <83541534+JanVGX@users.noreply.github.com>
2021-08-03 01:09:39 +0200

    Update help.js

--- Comandos/Usuarios/help.js
+++ Comandos/Usuarios/help.js
@@ -8,7 +8,7 @@ code: `
 
   $color[GREEN]
 
-  $description[**¡Chat Global es un bot de Discord por el que te podrás comunicar con más servidores que me hayan configurado por medio de un canal!**
+  $description[**All my Commands**
 
   - **c!setChannel:** Set the Global Chat to a channel.
 
@@ -16,7 +16,7 @@ code: `
   
   - **c!reset:** Delete your Global Chat.
 
-  - **Invite me:** https://discord.com/oauth2/authorize?client_id=867944692376039474&scope=bot%20applications.commands&permissions=8
+  - **Invite me:**[Global Bot](https://discord.com/oauth2/authorize?client_id=867944692376039474&scope=bot%20applications.commands&permissions=8)
 
   ]
 
