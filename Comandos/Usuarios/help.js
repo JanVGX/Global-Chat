2345e4244a0c2d315eee5703466c450be3877b87
JanVGX <83541534+JanVGX@users.noreply.github.com>
2021-08-03 00:35:27 +0200

    Update help.js

--- Comandos/Usuarios/help.js
+++ Comandos/Usuarios/help.js
@@ -10,19 +10,15 @@ code: `
 
   $description[**¡Chat Global es un bot de Discord por el que te podrás comunicar con más servidores que me hayan configurado por medio de un canal!**
 
-  - **c/setChannel:** Establece un canal de interchat para el servidor.
+  - **c!setChannel:** Set the Global Chat to a channel.
 
-  - **c/setInvite:** Establece una invitación de interchat para el servidor.
+  - **c!setInvite:** Set the Discord invite link on your Bot.
+  
+  - **c!reset:** Delete your Global Chat.
 
-  - **c/setColor:** Personaliza el color de tu embed para el interchat.
+  - **Invite me:** https://discord.com/oauth2/authorize?client_id=867944692376039474&scope=bot%20applications.commands&permissions=8
 
-  - **c/reset:** Desactiva el canal y la invitación de interchat en el servidor.
-
-  - **c/config:** Mira sobre que configuración de interchat está puesta en el servidor.
-
-  - **Invitame:** [Aquí](https://discord.com/oauth2/authorize?client_id=863880148822786078&scope=bot&permissions=8)
-
-  - **Soporte:** https://discord.gg/huguitis]
+  ]
 
   $suppressErrors
 
